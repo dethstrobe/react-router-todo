13733e388bf090c2cfe0f934237d6edbc83f77ca
dethstrobe <dethstrobe@gmail.com>
2025-10-21 16:32:23 -0600

    add very basic test and nextly generated doc file

---
+++ doc/docs/test2doc-remix-todo-app.md
@@ -0,0 +1,8 @@
+# REMIX TODO App
+
+## The app title
+
+When you navigate to `/todo`.
+The page should display the title of the app.
+![screenshot](./test2doc-1761085892947-1.png)
+
--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,11 +1,20 @@
 import { test, expect } from "@playwright/test"
+import { screenshot } from "@test2doc/playwright/screenshots"
 
 test.describe("REMIX TODO App", () => {
-  test("has title", async ({ page }) => {
-    await page.goto("/todo")
+  test("The app title", async ({ page }, pageInfo) => {
+    await test.step("When you navigate to `/todo`.", async () => {
+      await page.goto("/todo")
+    })
 
-    await expect(
-      page.getByRole("heading", { name: "A very simple TODO app" }),
-    ).toBeVisible()
+    await test.step("The page should display the title of the app.", async () => {
+      const header = page.getByRole("heading", {
+        name: "A very simple TODO app",
+      })
+      await screenshot(pageInfo, header, {
+        annotation: { text: "This is the title", showArrow: true },
+      })
+      await expect(header).toBeVisible()
+    })
   })
 })
